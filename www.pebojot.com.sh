55b599e2a066f14db93f8991f31e1cf640889a2a
workpebojot <pebojot.edison.work@outlook.com>
2021-03-30 08:40:57 +0800

    Update 0.7.0

---
+++ www.pebojot.com.sh
@@ -0,0 +1,1 @@
+alias done="git add . && git commit -m 'Update 0.7.0' && git push origin main && JEKYLL_ENV=production bundle exec jekyll build && cd _site && git add . && git commit -m 'Update 0.7.0' && git push deploy master && cd .."
